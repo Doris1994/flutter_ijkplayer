da734169c56133c4bbeb63636ff11e8f94ffa103
Doris1994 <sundeqi8086@163.com>
2019-11-21 17:26:52 +0800

    update example config to debug

--- example/lib/page/network.dart
+++ example/lib/page/network.dart
@@ -15,19 +15,21 @@ class _NetworkPageState extends State<NetworkPage> {
   void initState() {
     super.initState();
 
-    editingController.text =
-        "https://www.sample-videos.com/video123/mp4/720/big_buck_bunny_720p_30mb.mp4";
     // editingController.text =
+    //     "https://www.sample-videos.com/video123/mp4/720/big_buck_bunny_720p_30mb.mp4";
+    // // editingController.text =
     //     "http://img.ksbbs.com/asset/Mon_1703/05cacb4e02f9d9e.mp4";
 
-    editingController.text =
-        "https://media001.geekbang.org/f433fd1ce5e84d27b1101f0dad72a126/de563bb4aba94b5f95f448b33be4dd9f-9aede6861be944d696fe365f3a33b7b4-sd.m3u8";
+    // editingController.text =
+    //     "https://media001.geekbang.org/f433fd1ce5e84d27b1101f0dad72a126/de563bb4aba94b5f95f448b33be4dd9f-9aede6861be944d696fe365f3a33b7b4-sd.m3u8";
 
     mediaController
-        .setIjkPlayerOptions([TargetPlatform.android], createIJKOptions());
+        .addIjkPlayerOptions([TargetPlatform.android], createAndroidIJKOptions());
+    mediaController
+        .addIjkPlayerOptions([TargetPlatform.iOS], createiOSIJKOptions());
     // editingController.text = "http://222.207.48.30/hls/startv.m3u8";
 
-    // editingController.text = "rtmp://172.16.100.245:1935/live1";
+     editingController.text = "rtmp://192.168.1.2:1935/live";
 
     // editingController.text = "http://172.16.100.245:5000/meng.mp4";
     // editingController.text = "http://172.16.100.245:5000/sample1.mp4";
@@ -88,14 +90,48 @@ class _NetworkPageState extends State<NetworkPage> {
 }
 
 // the option is copied from ijkplayer example
-Set<IjkOption> createIJKOptions() {
+Set<IjkOption> createAndroidIJKOptions() {
   return <IjkOption>[
-    IjkOption(IjkOptionCategory.player, "mediacodec", 0),
+    IjkOption(IjkOptionCategory.player, "mediacodec", 1),
+    IjkOption(IjkOptionCategory.player, "mediacodec-auto-rotate", 1),
+    IjkOption(IjkOptionCategory.player, "mediacodec-handle-resolution-change", 1),
     IjkOption(IjkOptionCategory.player, "opensles", 0),
     IjkOption(IjkOptionCategory.player, "overlay-format", 0x32335652),
+
+    IjkOption(IjkOptionCategory.player, "enable-accurate-seek", 1),
+    IjkOption(IjkOptionCategory.player, "max-fps", 30),
     IjkOption(IjkOptionCategory.player, "framedrop", 1),
-    IjkOption(IjkOptionCategory.player, "start-on-prepared", 0),
-    IjkOption(IjkOptionCategory.format, "http-detect-range-support", 0),
-    IjkOption(IjkOptionCategory.codec, "skip_loop_filter", 48),
+    //IjkOption(IjkOptionCategory.player, "videotoolbox", 1),
+    //IjkOption(IjkOptionCategory.player, "videotoolbox-max-frame-width", 1920),
+    IjkOption(IjkOptionCategory.player, "max_cached_durtion", 100),
+    IjkOption(IjkOptionCategory.player, "infbuf", 1),
+    IjkOption(IjkOptionCategory.player, "packet-buffering", 0),
+
+    IjkOption(IjkOptionCategory.format, "probesize", 1024),
+    IjkOption(IjkOptionCategory.format, "max-buffer-size", 1024*3),
+    IjkOption(IjkOptionCategory.format, "analyzeduration", 1),
+    
+    IjkOption(IjkOptionCategory.codec, "skip_loop_filter", 0),
+    IjkOption(IjkOptionCategory.codec, "skip_frame", 8),
+  ].toSet();
+}
+
+Set<IjkOption> createiOSIJKOptions() {
+  return <IjkOption>[
+    IjkOption(IjkOptionCategory.player, "enable-accurate-seek", 1),
+    IjkOption(IjkOptionCategory.player, "max-fps", 30),
+    IjkOption(IjkOptionCategory.player, "framedrop", 1),
+    //IjkOption(IjkOptionCategory.player, "videotoolbox", 1),
+    //IjkOption(IjkOptionCategory.player, "videotoolbox-max-frame-width", 1920),
+    IjkOption(IjkOptionCategory.player, "max_cached_durtion", 100),
+    IjkOption(IjkOptionCategory.player, "infbuf", 1),
+    IjkOption(IjkOptionCategory.player, "packet-buffering", 0),
+
+    IjkOption(IjkOptionCategory.format, "probesize", 1024),
+    IjkOption(IjkOptionCategory.format, "max-buffer-size", 1024*3),
+    IjkOption(IjkOptionCategory.format, "analyzeduration", 1),
+    
+    IjkOption(IjkOptionCategory.codec, "skip_loop_filter", 0),
+    IjkOption(IjkOptionCategory.codec, "skip_frame", 8),
   ].toSet();
 }

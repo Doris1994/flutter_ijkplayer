0e98a0bcd40531b8d4c7086a9269ec51b5763698
Doris1994 <sundeqi8086@163.com>
2020-07-14 17:49:52 +0800

    update example ios project config

--- example/lib/page/network.dart
+++ example/lib/page/network.dart
@@ -5,32 +5,32 @@ import 'package:ijkplayer_example/i18n/i18n.dart';
 
 import 'video_player/video_player.dart';
 
-
 class NetworkPage extends StatelessWidget {
   final stack = Stack(
     children: <Widget>[
-      VideoPlayerPage(url:'rtmp://192.168.1.151:1935/live1/room',isLive: true),
+      VideoPlayerPage(url: 'rtsp://192.168.1.160:554', isLive: true),
       //Container(color: Colors.lightBlue,height: 48,),
       SafeArea(
-        child:
-      BackButton(color: Colors.white,) ,
+        child: BackButton(
+          color: Colors.white,
+        ),
       )
     ],
-  ); 
+  );
 
   @override
-  StatelessElement createElement(){
+  StatelessElement createElement() {
     hideStatusBar();
     return super.createElement();
   }
 
   static void hideStatusBar() async {
     await SystemChrome.setEnabledSystemUIOverlays([]);
-  } 
+  }
 
   static void showStatusBar() async {
     await SystemChrome.setEnabledSystemUIOverlays(SystemUiOverlay.values);
-  } 
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -40,13 +40,13 @@ class NetworkPage extends StatelessWidget {
   }
 }
 
-
 // the option is copied from ijkplayer example
 Set<IjkOption> createAndroidIJKOptions() {
   return <IjkOption>[
     IjkOption(IjkOptionCategory.player, "mediacodec", 1),
     IjkOption(IjkOptionCategory.player, "mediacodec-auto-rotate", 1),
-    IjkOption(IjkOptionCategory.player, "mediacodec-handle-resolution-change", 1),
+    IjkOption(
+        IjkOptionCategory.player, "mediacodec-handle-resolution-change", 1),
     IjkOption(IjkOptionCategory.player, "opensles", 0),
     IjkOption(IjkOptionCategory.player, "overlay-format", 0x32335652),
 
@@ -60,9 +60,9 @@ Set<IjkOption> createAndroidIJKOptions() {
     IjkOption(IjkOptionCategory.player, "packet-buffering", 0),
 
     IjkOption(IjkOptionCategory.format, "probesize", 1024),
-    IjkOption(IjkOptionCategory.format, "max-buffer-size", 1024*3),
+    IjkOption(IjkOptionCategory.format, "max-buffer-size", 1024 * 3),
     IjkOption(IjkOptionCategory.format, "analyzeduration", 1),
-    
+
     IjkOption(IjkOptionCategory.codec, "skip_loop_filter", 0),
     IjkOption(IjkOptionCategory.codec, "skip_frame", 8),
   ].toSet();
@@ -80,9 +80,9 @@ Set<IjkOption> createiOSIJKOptions() {
     IjkOption(IjkOptionCategory.player, "packet-buffering", 0),
 
     IjkOption(IjkOptionCategory.format, "probesize", 1024),
-    IjkOption(IjkOptionCategory.format, "max-buffer-size", 1024*3),
+    IjkOption(IjkOptionCategory.format, "max-buffer-size", 1024 * 3),
     IjkOption(IjkOptionCategory.format, "analyzeduration", 1),
-    
+
     IjkOption(IjkOptionCategory.codec, "skip_loop_filter", 0),
     IjkOption(IjkOptionCategory.codec, "skip_frame", 8),
   ].toSet();
